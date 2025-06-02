3594f3c45b847dc5dc85ee274c9911ed620f1ae1
Theo <>
2025-06-02 13:56:17 +0200

    First Comm

---
+++ Assets/_/Features/Lamp/Runtime/LampBehaviour.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Diagnostics;
+using Tools;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class LampBehaviour : MonoBehaviour
+{
+    private PhantomBehaviour _phantomBehaviour;
+    public LampConfig _lampConfig;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_phantomBehaviour == null)
+        {
+            _phantomBehaviour = other.GetComponent<PhantomBehaviour>();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Other " + other.name + "Damage: " + _lampConfig.m_damage);
+        
+        //_phantomBehaviour.TakeDamage(_lampConfig.m_damage);
+        
+        var bh = other.GetComponent<PhantomBehaviour>();
+        bh.TakeDamage(_lampConfig.m_damage);
+    }
+   
+}
--- Assets/_/Features/Player/Runtime/PlayerFov.cs
+++ Assets/_/Features/Player/Runtime/PlayerFov.cs
@@ -34,27 +34,56 @@ namespace Player.Runtime
 
         private void FieldOfViewCheck()
         {
-            Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+            //Collider2D[] rangeChecks = Physics2D.OverlapCircleAll(transform.position, radius, targetMask);
 
-            if (rangeChecks.Length != 0)
-            {
-                Transform target = rangeChecks[0].transform;
-                Vector3 directionToTarget = (target.position - transform.position).normalized;
+            /*if (rangeChecks.Length != 0)
+            {*/
 
-                if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+                Vector3 mousePos = Input.mousePosition;
+                Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0f));
+
+                // Optional: Zero out the Z-coordinate for 2D
+                /*
+                mouseWorldPos.z = 0f;
+                */
+                
+                Debug.Log(mouseWorldPos);
+                Vector2 directionToTarget = (mouseWorldPos - (Vector2)transform.position);
+
+                Vector2 facing = GetFacingDirection();
+                float angleToTarget = Vector2.Angle(facing, directionToTarget);
+
+                if (angleToTarget < angle / 2)
                 {
-                    float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                    float distanceToTarget = Vector2.Distance(transform.position, mouseWorldPos);
 
-                    if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    // Utilisation de Physics2D.Raycast
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask);
+                    
+                    if (hit.collider == null)
                         canSeePlayer = true;
                     else
                         canSeePlayer = false;
                 }
                 else
                     canSeePlayer = false;
-            }
+            /*}
             else if (canSeePlayer)
-                canSeePlayer = false;
+                canSeePlayer = false;*/
+        }
+
+        // Méthode pour obtenir la direction vers laquelle l'objet fait face en 2D
+        private Vector2 GetFacingDirection()
+        {
+            // Option 1: Basé sur la rotation Z (le plus commun en 2D)
+            float angle = transform.eulerAngles.y * Mathf.Deg2Rad;
+            return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            
+            // Option 2: Si votre objet utilise transform.right comme direction
+            // return transform.right;
+            
+            // Option 3: Si votre objet utilise transform.up comme direction
+            // return transform.up;
         }
     }
 }
