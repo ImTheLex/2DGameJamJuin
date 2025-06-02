5e28061b5f0da14f71bfba34a9fd9e99ebb9569a
Theo <>
2025-06-02 16:44:44 +0200

    Added Transition using scene buttons

--- Assets/_/Features/Scenes/Runtime/SceneBehaviour.cs
+++ Assets/_/Features/Scenes/Runtime/SceneBehaviour.cs
@@ -3,8 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneBehaviour : MonoBehaviour
 {
+    private Scene _previousScene;
     public void StartGame()
     {
+        _previousScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene("Maingame_scene");
+        
+    }
+
+    public void ShowScore()
+    {
+        _previousScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("Scoring_scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Return()
+    {
+        //SceneManager.LoadScene(_previousScene.buildIndex);
+        SceneManager.LoadScene("mainmenu_scene");
     }
 }
